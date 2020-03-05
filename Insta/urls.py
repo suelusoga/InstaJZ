a5e33155be83cd0d0118a0b0e5e659d63e46c700
suelusoga <sue.snowday@gmail.com>
2020-03-05 11:44:20 -0800

    test

--- Insta/urls.py
+++ Insta/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from Insta.views import HelloWorld
+from Insta.views import HelloWorld, PostsView, PostDetailView, PostCreateView
+
 
 urlpatterns = [
-    path('', HelloWorld.as_view(), name='HelloWorld')
+    path('', HelloWorld.as_view(), name='HelloWorld'),
+    path('posts/', PostsView.as_view(), name='posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='make_post'),
 ]
--- Insta/views.py
+++ Insta/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from Insta.models import Post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 class HelloWorld(TemplateView):
     template_name = 'test.html'
+
+
+class PostsView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
