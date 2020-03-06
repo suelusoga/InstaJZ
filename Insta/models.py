b3f63fd5922bd2aba8e55d3396b869db17128716
suelusoga <sue.snowday@gmail.com>
2020-03-05 21:04:38 -0800

    authentication finished

--- Insta/models.py
+++ Insta/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from imagekit.models import ProcessedImageField
 from django.urls import reverse
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 
@@ -22,6 +23,16 @@ class PostTwo(models.Model):
     title = models.TextField(blank=True, null=True)
     image = ProcessedImageField(
         upload_to='static/images/posts',
+        format='JPEG',
+        options={'quality': 100},
+        blank=True,
+        null=True
+    )
+
+
+class InstaUser(AbstractUser):
+    profile_pic = ProcessedImageField(
+        upload_to='static/images/profiles',
         format='JPEG',
         options={'quality': 100},
         blank=True,
--- Insta/views.py
+++ Insta/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
 from Insta.models import Post
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
+from Insta.forms import CustomUserCreationForm
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class HelloWorld(TemplateView):
@@ -20,7 +24,26 @@ class PostDetailView(DetailView):
     template_name = 'post_detail.html'
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_create.html'
     fields = '__all__'
+    login_url = 'login'
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title']
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy("posts")
+
+
+class SignUp(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy("login")
