17cb912f68e2a74e98b3e8d529c744aeed10974f
suelusoga <sue.snowday@gmail.com>
2020-03-05 21:04:21 -0800

    authentication finished

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from Insta.models import Post, PostTwo
+from Insta.models import Post, PostTwo, InstaUser
 
 admin.site.register(Post)
 admin.site.register(PostTwo)
+admin.site.register(InstaUser)
